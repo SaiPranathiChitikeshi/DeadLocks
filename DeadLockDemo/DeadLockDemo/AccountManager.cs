e752c93aca3a4b1b06bf0c6a0541a8bd6e9a3e2e
SaiPranathiChitikeshi <75035288+SaiPranathiChitikeshi@users.noreply.github.com>
2021-01-06 19:38:15 +0530

    Add files via upload

---
+++ DeadLockDemo/DeadLockDemo/AccountManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+namespace DeadLockDemo
+{
+    public class AccountManager
+    {
+        private Account FromAccount;
+        private Account ToAccount;
+        private double TransferAmount;
+        public AccountManager(Account AccountFrom, Account AccountTo, double AmountTransfer)
+        {
+            this.FromAccount = AccountFrom;
+            this.ToAccount = AccountTo;
+            this.TransferAmount = AmountTransfer;
+        }
+        public void FundTransfer()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} trying to acquire lock on {FromAccount.ID}");
+
+            lock (FromAccount)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} acquired lock on {FromAccount.ID}");
+                Console.WriteLine($"{Thread.CurrentThread.Name} trying to transfer");
+                Thread.Sleep(3000);
+                Console.WriteLine($"{Thread.CurrentThread.Name} trying to acquire lock on {ToAccount.ID}");
+
+                if (Monitor.TryEnter(ToAccount, 3000))
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name} acquired lock on {ToAccount.ID}");
+                    try
+                    {
+                        FromAccount.WithdrawMoney(TransferAmount);
+                        ToAccount.DepositMoney(TransferAmount);
+                    }
+                    finally
+                    {
+                        Monitor.Exit(ToAccount);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name} Unable to acquire lock on {ToAccount.ID}, So existing.");
+                }
+            }
+        }
+    }
+}
---
+++ deadlockincsharp/deadlockincsharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+namespace deadlockincsharp
+{
+    public class Program
+    {
+        static readonly object firstLock = new object();
+        static readonly object secondLock = new object();
+        static void ThreadJob()
+        {
+            Console.WriteLine("\t\tLocking firstLock");
+            lock (firstLock)
+            {
+                Console.WriteLine("\t\tLocked firstLock");
+                
+                Thread.Sleep(1000);
+                Console.WriteLine("\t\tLocking secondLock");
+                lock (secondLock)
+                {
+                    Console.WriteLine("\t\tLocked secondLock");
+                }
+                Console.WriteLine("\t\tReleased secondLock");
+            }
+            Console.WriteLine("\t\tReleased firstLock");
+        }
+        static void Main()
+        {
+            new Thread(new ThreadStart(ThreadJob)).Start();
+            
+            Thread.Sleep(500);
+            Console.WriteLine("Locking secondLock");
+            lock (secondLock)
+            {
+                Console.WriteLine("Locked secondLock");
+                Console.WriteLine("Locking firstLock");
+                lock (firstLock)
+                {
+                    Console.WriteLine("Locked firstLock");
+                }
+                Console.WriteLine("Released firstLock");
+            }
+            Console.WriteLine("Released secondLock");
+            Console.Read();
+        }
+    }
+}
